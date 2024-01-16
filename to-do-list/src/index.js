1082e782f4c096952a710a28ecfe6e85b2bfb145
chr0405 <ch0nn@naver.com>
2024-01-17 01:16:42 +0900

    pure redux로 to do list 만들기

---
+++ to-do-list/src/index.js
@@ -0,0 +1,77 @@
+import { legacy_createStore } from "redux";
+
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+// li를 담아야 하니 state를 빈 배열로
+// mutating state를 쓰지 x, 새로운 object를 return 해야 함
+const reducer = (state = [], action) => {
+  console.log(state, action);
+  switch (action.type) {
+    case ADD_TODO :
+      // ...state는 ES6로 모든 state array의 contents를 의미
+      return [{text : action.text, id: Date.now() }, ...state];
+    case DELETE_TODO :
+      // filter()
+      return state.filter(toDo => toDo.id !== action.id);
+    default :
+      return state;
+  }
+};
+
+const store = legacy_createStore(reducer);
+
+const deleteToDo = id => {
+  return{
+    type : DELETE_TODO,
+    id
+  };
+}
+
+const dispatchDeleteToDo = e => {
+  // console.log(e.target.parentNode.id);
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteToDo(id));
+}
+
+const addToDo = text => {
+  return{
+    type: ADD_TODO,
+    text
+  };
+};
+
+const painToDos = () => {
+  const toDos = store.getState();
+  // .forEach 배열의 한 요소마다에 대해서
+  ul.innerHTML="";
+  toDos.forEach(toDo=>{
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchDeleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+}
+
+store.subscribe(painToDos);
+
+const dispatchAddToDo = text => {
+  store.dispatch(addToDo(text));
+};
+
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  dispatchAddToDo(toDo);
+};
+
+form.addEventListener("submit", onSubmit);
