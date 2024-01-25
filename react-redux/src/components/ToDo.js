4c29e9481dd268074225b3c73c0b656b6d822583
chr0405 <ch0nn@naver.com>
2024-01-26 00:59:00 +0900

    완강!!

--- react-redux/src/components/ToDo.js
+++ react-redux/src/components/ToDo.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
-import { actionCreators } from "../store";
-import { Link } from 'react-router-dom';
+import { remove } from "../store";
+import { Link } from "react-router-dom";
 
 function ToDo({ text, onBtnClick, id }) {
   return (
     <li>
-      <Link to={`/${id}`}>
-        {text}
-      </Link>
+      <Link to={`/${id}`}>{text}</Link>
       <button onClick={onBtnClick}>DEL</button>
     </li>
   );
@@ -16,11 +14,8 @@ function ToDo({ text, onBtnClick, id }) {
 
 function mapDispatchToProps(dispatch, ownProps) {
   return {
-    // id를 가지고 있으니 다른 arg 가 필요 없다
-    onBtnClick: () => dispatch(actionCreators.deleteToDo(ownProps.id))
+    onBtnClick: () => dispatch(remove(ownProps.id))
   };
 }
 
-
-// state는 신경 쓰지 않고 dispatch만
 export default connect(null, mapDispatchToProps)(ToDo);
--- react-redux/src/store.js
+++ react-redux/src/store.js
@@ -1,70 +1,17 @@
-// import { legacy_createStore as createStore } from 'redux';
-import { createAction, createReducer, configureStore } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-// (1)
-// const ADD = "ADD";
-// const DELETE = "DELETE";
+// createSlice는 자동적으로 Reducer를 제공
+const toDos = createSlice({
+  name: "toDosReducer",
+  initialState: [],
+  reducers: {
+    add: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    },
+    remove: (state, action) => state.filter(toDo => toDo.id !== action.payload)
+  }
+});
 
-// const addToDo = text => {
-//   return {
-//     type: ADD,
-//     text
-//   };
-// };
+export const { add, remove } = toDos.actions;
 
-// const deleteToDo = id => {
-//   return {
-//     type: DELETE,
-//     id: parseInt(id)
-//   };
-// };
-
-// (2)
-// function임
-const addToDo = createAction("ADD");
-const deleteToDo = createAction("DELETE");
-
-// (1)
-// const reducer = (state = [], action) => {
-//   switch (action.type) {
-//     case addToDo.type:
-//       return [{ text: action.payload, id: Date.now() }, ...state];
-//     case deleteToDo.type:
-//       return state.filter(toDo => toDo.id !== action.payload);
-//     default:
-//       return state;
-//   }
-// };
-
-// (3)
-// state를 mutate 할 수 있다 (이 경우엔 return 하면 안 됨)
-// 새로운 state를 return 할 수도 있음
-const reducer = createReducer([], (builder) => {
-  builder
-  .addCase(addToDo, (state, action) => {
-  state.push({ text: action.payload, id: Date.now() });
-  })
-  .addCase(deleteToDo, (state, action) => {
-    return state.filter((toDo) => toDo.id !== action.payload);
-  });
-  });
-
-// (2)
-// const reducer = createReducer([], {
-//   [addToDo]: (state, action) => {
-//     state.push({ text: action.payload, id: Date.now() });
-//   },
-//   [deleteToDo]: (state, action) =>
-//     state.filter(toDo => toDo.id !== action.payload)
-// });
-
-// const store = createStore(reducer);
-
-const store = configureStore( { reducer });
-
-export const actionCreators = {
-  addToDo,
-  deleteToDo
-};
-
-export default store;
+export default configureStore({ reducer: toDos.reducer });
