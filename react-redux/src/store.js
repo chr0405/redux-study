ad7ce0e432fcc58a9c5fd4a7f445228d196b296e
chr0405 <ch0nn@naver.com>
2024-01-26 00:34:35 +0900

    #4.3

--- react-redux/src/store.js
+++ react-redux/src/store.js
@@ -1,34 +1,66 @@
-import { legacy_createStore as createStore } from "redux";
+// import { legacy_createStore as createStore } from 'redux';
+import { createAction, createReducer, configureStore } from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
+// (1)
+// const ADD = "ADD";
+// const DELETE = "DELETE";
 
-const addToDo = text => {
-  return {
-    type: ADD,
-    text
-  };
-};
+// const addToDo = text => {
+//   return {
+//     type: ADD,
+//     text
+//   };
+// };
 
-const deleteToDo = id => {
-  return {
-    type: DELETE,
-    id: parseInt(id)
-  };
-};
+// const deleteToDo = id => {
+//   return {
+//     type: DELETE,
+//     id: parseInt(id)
+//   };
+// };
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
-    case DELETE:
-      return state.filter(toDo => toDo.id !== action.id);
-    default:
-      return state;
-  }
-};
+// (2)
+// function임
+const addToDo = createAction("ADD");
+const deleteToDo = createAction("DELETE");
 
-const store = createStore(reducer);
+// (1)
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case addToDo.type:
+//       return [{ text: action.payload, id: Date.now() }, ...state];
+//     case deleteToDo.type:
+//       return state.filter(toDo => toDo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
+
+// (3)
+// state를 mutate 할 수 있다 (이 경우엔 return 하면 안 됨)
+// 새로운 state를 return 할 수도 있음
+const reducer = createReducer([], (builder) => {
+  builder
+  .addCase(addToDo, (state, action) => {
+  state.push({ text: action.payload, id: Date.now() });
+  })
+  .addCase(deleteToDo, (state, action) => {
+    return state.filter((toDo) => toDo.id !== action.payload);
+  });
+  });
+
+// (2)
+// const reducer = createReducer([], {
+//   [addToDo]: (state, action) => {
+//     state.push({ text: action.payload, id: Date.now() });
+//   },
+//   [deleteToDo]: (state, action) =>
+//     state.filter(toDo => toDo.id !== action.payload)
+// });
+
+// const store = createStore(reducer);
+
+const store = configureStore( { reducer });
 
 export const actionCreators = {
   addToDo,
